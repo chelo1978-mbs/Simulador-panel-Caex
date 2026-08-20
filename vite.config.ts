0f86b23cabf016aa15ac24dcd55b07465ac173fc
chelo1978-mbs <chelo1978@gmail.com>
2026-08-20 16:21:17 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/PANEL-FINAL/',
+    base: '/Simulador-panel-Caex/',
 
     plugins: [react(), tailwindcss()],
 
